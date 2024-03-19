9567adb34cf72e2e1e5a64e8a28a15cc38bdd6f3
icedcacao <icedcacaogaming@gmail.com>
2024-03-18 22:55:51 -0700

    Fixing to base64 image

--- index.js
+++ index.js
@@ -1,56 +1,95 @@
 import express from "express";
-import multer from "multer";
+import fs from "fs";
+import morgan from "morgan";
 import "dotenv/config";
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now();
-    const saveName = (uniqueSuffix + "-" + file.originalname).replace(
-      /\s/g,
-      ""
-    );
-    cb(null, saveName);
-  },
-});
-
-const whiteListMime = ["image/jpeg", "image/jpg", "image/png"];
-
-async function fileFilter(req, file, cb) {
-  if (!whiteListMime.includes(file.mimetype)) {
-    return cb(new Error("Bad Request!"));
-  }
-  cb(null, true);
-}
-
-const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
 app.use(express.static("public"));
+app.use(morgan("dev"));
 
-const authApiKey = (req, res, next) => {
+const SIGNATURE = {
+  iVBORw0KGgo: "png",
+  "/9j/4A": "jpg",
+};
+
+function getCurrentDateTimeString() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  return `${year}${month}${day}_${hours}_${minutes}_${seconds}`;
+}
+
+function generateRandomString(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+function checkSignature(b64) {
+  for (const sign in SIGNATURE) {
+    if (b64.startsWith(sign)) {
+      return SIGNATURE[sign];
+    }
+  }
+  return "";
+}
+
+const checkApiAuthMiddleware = (req, res, next) => {
   const apiKey = req.headers["x-api-key"];
   if (!apiKey || apiKey != process.env.API_KEY) {
-    return res.status(401).json({ message: "Unauthorized!" });
+    return res.status(401).json({ message: "Unauthorized" });
   }
   next();
 };
 
-app.post("/upload", authApiKey, (req, res) => {
-  upload.single("image")(req, res, (err) => {
-    if (err) {
-      return res.status(400).json({ message: "Bad Request!" });
-    }
-    return res.json({ path: req.file.filename });
-  });
-});
+const removeBase64InfoMiddleware = async (req, res, next) => {
+  let info = req.body.image;
+  let result = await info.split(",").pop();
+  req.body.image = result;
+  next();
+};
+
+const checkSignatureMiddleware = (req, res, next) => {
+  let image = req.body.image;
+  if (checkSignature(image) === "") {
+    return res.status(400).json({ message: "Bad Request" });
+  }
+  next();
+};
+
+app.post(
+  "/upload",
+  checkApiAuthMiddleware,
+  removeBase64InfoMiddleware,
+  checkSignatureMiddleware,
+  (req, res) => {
+    let name =
+      req.body.name != undefined
+        ? `${req.body.name}_`
+        : `${generateRandomString(10)}_`;
+    let image = req.body.image;
+    name += `${getCurrentDateTimeString()}`;
+    let urlName = `${name}.${checkSignature(image)}`;
+    fs.writeFile(`./public/${urlName}`, image, "base64", function (err) {
+      console.error(err);
+    });
+    return res.status(200).json({
+      imageUrl: urlName,
+    });
+  }
+);
+
 app.listen(process.env.PORT, function (err) {
   if (err) console.log(err);
   console.log(`Server is running at ${process.env.PORT}`);
 });
-
-// ATTENTION! NEED PROPER ERROR HANDLING ON MULTER!!!
